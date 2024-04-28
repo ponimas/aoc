836405d873bb9f62b9ca62f7fb605262f2d083c6
Alexander Ponimaskin <ponimaskin@gmail.com>
2024-04-28 19:50:29 +0200

    9.2

--- 9.clj
+++ 9.clj
@@ -3,18 +3,35 @@
 (defn pairs [l]
   (map vector l (rest l)))
 
+(def data
+  (-> (slurp "in/9.txt")
+    (str/split #"\n")
+    (->> (map #(->>
+                 (str/split % #"\s")
+                 (map parse-long))))))
+
+;; 1
+
 (defn first-solution [ss]
   (if (apply = ss)
     (last ss)
     (let [xx
           (+ (last ss)
-            (x (map (fn [[f s]] (- s f)) (pairs ss))))]
+            (first-solution (map (fn [[f s]] (- s f)) (pairs ss))))]
       xx)))
 
+(apply + (for [l data] (first-solution l)))
 
-(let [data (-> (slurp "in/9.txt")
-             (str/split #"\n")
-             (->> (map #(->>
-                          (str/split % #"\s")
-                          (map parse-long)))))]
-  (apply + (for [l data] (first-solution l))))
+;; 2
+
+(defn second-solution [ss]
+  (println ss)
+  (if (apply = ss)
+    (first ss)
+    (let [xx
+          (- (first ss)
+            (second-solution (map (fn [[f s]] (- s f)) (pairs ss))))]
+      (println xx)
+      xx)))
+
+(apply + (for [l data] (second-solution l)))
