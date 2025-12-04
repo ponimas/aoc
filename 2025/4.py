7baafb0c6f048b582501d5f910a11e76f3f95e7f
Aleksandr Ponimaskin <aleksandr.ponimaskin@zalando.de>
2025-12-04 08:13:37 +0100

    4.1

---
+++ 2025/4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+f = 'in/4.test.txt'
+f = 'in/4.txt'
+
+data = open(f).read().strip().split("\n")
+
+xx = len(data[0]) -1
+yy = len(data) -1
+
+def n(y, x):
+    for dx in [x - 1, x, x + 1]:
+        for dy in [y - 1, y, y + 1]:
+            if 0 <= dx <= xx and 0 <= dy <= yy and (dx != x or dy != y):
+                yield (dy, dx)
+
+c = 0                
+for y, l in enumerate(data):
+    for x, s in enumerate(l):
+        if data[y][x] != '@':
+            continue
+        ns = [(dx, dy) for [dy, dx] in n(y, x) if data[dy][dx] == '@']
+        # print(x, y, ns)        
+        if len(ns) < 4:
+            c += 1
+
+print(c)
+            
